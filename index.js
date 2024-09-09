539fc590cd09b8823d5ac378e2d9c52f5eb85e09
Bruno <brunoanderson.macedorosendo@gmail.com>
2024-09-09 20:23:45 -0300

    fundamentos002

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-// hello world
-let mensagem = "Olá, mundo";
-console.log(mensagem);
+// arrays, objetos
+let meta = {
+      value: "Ler um livro por mês",
+      address: 2,
+      checked: false,
+      isChecked: (info) => {
+            console.log(info)
+      }
+};
+
+meta.value = "não é mais ler um livro";
+meta.isChecked(meta.value);
+
+const criarMeta = () => {}
+
+// function criarMeta(){}
